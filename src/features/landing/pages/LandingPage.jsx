4ca2af49ef1b3c64516bcb151366e81a119ad6fe
Denzel <denzeltifferleiva@gmail.com>
2026-07-27 18:08:16 -0600

    refactor(routes): mover transportista a ruta propia
    Se libera "/" para la landing publica de PIXCA y TransportistaHome
    pasa a vivir en /transportista. Se quita el alias duplicado
    /transportistahome y el export default sin uso que colgaba de
    AppRoutes.jsx.

---
+++ src/features/landing/pages/LandingPage.jsx
@@ -0,0 +1,7 @@
+export const LandingPage = () => {
+  return (
+    <div>
+      <p>Landing PIXCA en construccion</p>
+    </div>
+  )
+}
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,16 +1,13 @@
 // src/routes/AppRoutes.jsx
 import { Routes, Route } from 'react-router-dom'
 import { TransportistaHome } from '../features/transportista/pages/TransportistaHome'
-import App from '../features/home/App'
+import { LandingPage } from '../features/landing/pages/LandingPage'
 
 export const AppRoutes = () => {
   return (
     <Routes>
-      {/* Definimos que cuando en el navegador escribas /pedidos-pendientes, cargue esa página */}
-      <Route path="/" element={<TransportistaHome/>} />
-      <Route path="/transportistahome" element={<TransportistaHome/>} />
+      <Route path="/" element={<LandingPage/>} />
+      <Route path="/transportista" element={<TransportistaHome/>} />
     </Routes>
   )
 }
-
-export default App
